e81b3073a3e6d08c6d2db9d089d22db67accc0e5
Herbert97 <falcon9718@hotmail.com>
2022-03-07 00:13:01 -0600

    tarea4

--- p56-secuencia-de-renglonesV2.py
+++ p56-secuencia-de-renglonesV2.py
@@ -2,8 +2,6 @@
 
 n = int(input('Cuantos renglones ? '))
 for i in range(1,n+1):
-    k=0
     for j in range(1,i+1):
-        k+=1
-        print(k, end="")
+        print(j, end="")
     print("\r")
