e885ef215239938a746742a4d97dc3cce4e1079f
Herbert97 <falcon9718@hotmail.com>
2022-04-12 01:01:39 -0500

    tarea5

--- p59-vocales_consonantes.py
+++ p59-vocales_consonantes.py
@@ -1,16 +1,13 @@
 # contar vocales y consonantes
 frase = input('Dame una frase ? ')
 voc=cons=0
-voc1=[]
-con1=[]
 print(frase)
 print(f'La frase tiene {len(frase)} caracteres')
 for c in frase:
     if c.isalpha():
         if c in 'aeiou':
             voc += 1
-            voc1+=c
         else:
             cons += 1
-print(f'Vocales = {voc} estas son las vocales : {voc1}')
+print(f'Vocales = {voc}')
 print(f'Consonantes = {cons}')
