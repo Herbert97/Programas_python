c677072deb1d5969018eb07e04e1a67e556bcea9
Herbert97 <falcon9718@hotmail.com>
2022-04-19 13:59:10 -0500

    Actividad9

---
+++ p92-nombres-y-edades.py
@@ -0,0 +1,11 @@
+# Nombres y edades
+datos = {}
+print('Introduce nombres y edades * para terminar')
+while True:
+    nombre = input('Dame el nombre ? ')
+    if nombre!='*':
+        edad = int(input('Edad ? '))
+        datos[nombre]=edad
+    else:
+        break
+print(f'El diccionario de datos creado: {datos}')
